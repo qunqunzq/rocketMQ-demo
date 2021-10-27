c97a1ae402d8a114108af84b474ecee097f79909
qun <qunqunzq@126.com>
2021-10-27 17:44:25 +0800

    netty

---
+++ SC-rocketmq/src/main/java/com/roy/scrocket/nio/NettyClient.java
@@ -0,0 +1,37 @@
+package com.roy.scrocket.nio;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @author 张群
+ * @version 1.0
+ * @date 2021/10/27 4:43 下午
+ */
+public class NettyClient {
+    public static void main(String[] args) {
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group).channel(NioSocketChannel.class).handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new NettyClientHandler());
+                }
+            });
+            final ChannelFuture sync = bootstrap.connect("127.0.0.1", 9000).sync();
+            sync.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
---
+++ SC-rocketmq/src/main/java/com/roy/scrocket/nio/NettyServer.java
@@ -0,0 +1,41 @@
+package com.roy.scrocket.nio;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author 张群
+ * @version 1.0
+ * @date 2021/10/27 4:26 下午
+ */
+public class NettyServer {
+    public static void main(String[] args)  {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workGroup = new NioEventLoopGroup(2);
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        try {
+            bootstrap.group(bossGroup,workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG,1024)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new NettyServerHandler());
+                        }
+                    });
+            final ChannelFuture sync = bootstrap.bind(9000).sync();
+            sync.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+}
